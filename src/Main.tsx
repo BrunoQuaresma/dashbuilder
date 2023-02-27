9918e7dab43fcdb2f62ba1a39601e89ecc8edd43
brunoquaresma <bruno_nonato_quaresma@hotmail.com>
2023-02-27 18:48:55 -0300

    refactor: Minor spacing adjustments

--- src/Main.tsx
+++ src/Main.tsx
@@ -5,5 +5,5 @@ import { SIDE_PADDING } from "./constants";
 export type MainProps = ComponentProps<typeof Box>;
 
 export const Main: FC<MainProps> = (boxProps) => {
-  return <Box as="main" px={SIDE_PADDING} py={10} {...boxProps} />;
+  return <Box as="main" px={SIDE_PADDING} py={12} {...boxProps} />;
 };
