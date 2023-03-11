f527d8a13714b641d9f4c1a1481a97c7e1a6cba1
brunoquaresma <bruno_nonato_quaresma@hotmail.com>
2023-03-11 15:35:17 -0300

    chore: Setup build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "dashy",
+      formats: ["es", "umd"],
+      fileName: (format) => `dashy.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "@chakra-ui/react"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "@chakra-ui/react": "ChakraUI",
+        },
+      },
+    },
+  },
+});
